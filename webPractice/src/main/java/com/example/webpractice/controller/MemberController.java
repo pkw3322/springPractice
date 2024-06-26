b74cbcddf14db0345eb94beece1e672552ba497a
pkw3322 <pkw3322@naver.com>
2024-06-26 17:11:43 +0900

    feat: 회원 가입 화면

--- webPractice/src/main/java/com/example/webpractice/controller/MemberController.java
+++ webPractice/src/main/java/com/example/webpractice/controller/MemberController.java
@@ -1,8 +1,13 @@
 package com.example.webpractice.controller;
 
+import com.example.webpractice.domain.Member;
 import com.example.webpractice.service.MemberService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -13,4 +18,23 @@ public class MemberController {
     this.memberService = memberService;
   }
 
+  @GetMapping(value = "/members/new")
+  public String createForm() {
+    return "members/createMemberForm";
+  }
+
+  @PostMapping(value = "/members/new")
+  public String create(MemberForm form) {
+    Member member = new Member();
+    member.setName(form.getName());
+    memberService.join(member);
+    return "redirect:/";
+  }
+
+  @GetMapping(value = "/members")
+  public String list(Model model) {
+    List<Member> members = memberService.findMembers();
+    model.addAttribute("members", members);
+    return "members/memberList";
+  }
 }
---
+++ webPractice/src/main/java/com/example/webpractice/controller/MemberForm.java
@@ -0,0 +1,10 @@
+package com.example.webpractice.controller;
+
+public class MemberForm {
+  private String name;
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  } }
