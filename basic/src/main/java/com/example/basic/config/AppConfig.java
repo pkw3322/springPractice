dac8ede6abeb330b567be93de89925a7f6855e0b
pkw3322 <pkw3322@naver.com>
2024-07-02 17:51:38 +0900

    refactor: AppConfig.java

--- basic/src/main/java/com/example/basic/config/AppConfig.java
+++ basic/src/main/java/com/example/basic/config/AppConfig.java
@@ -1,6 +1,9 @@
 package com.example.basic.config;
 
+import com.example.basic.discount.DiscountPolicy;
 import com.example.basic.discount.FixDiscountPolicy;
+import com.example.basic.discount.RateDiscountPolicy;
+import com.example.basic.member.MemberRepository;
 import com.example.basic.member.MemberService;
 import com.example.basic.member.MemberServiceImpl;
 import com.example.basic.member.MemoryMemberRepository;
@@ -8,12 +11,23 @@ import com.example.basic.order.OrderService;
 import com.example.basic.order.OrderServiceImpl;
 
 public class AppConfig {
+  //DIP, OCP 규칙 준수 repository 바꾼다면 memberRepository() 만 수정하면 된다.
 
   public MemberService memberService() {
-    return new MemberServiceImpl(new MemoryMemberRepository());
+    return new MemberServiceImpl(memberRepository());
   }
 
   public OrderService orderService() {
-    return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    return new OrderServiceImpl(memberRepository(), discountPolicy());
+  }
+
+  private MemberRepository memberRepository() {
+    return new MemoryMemberRepository();
+  }
+
+  private DiscountPolicy discountPolicy() {
+//    return new FixDiscountPolicy();
+    // To change discount policy, only this line needs to be changed.
+    return new RateDiscountPolicy();
   }
 }
